f035f23dd49fb8138b8a3730dca9e96dfca8ab7c
mohitsinghal1206 <mohitsinghal2003.ms@gmail.com>
2024-10-03 05:57:08 +0530

    Updated favicons and removed default React favicon

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
-    return (
-        <div>
-            This is an about component 
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum corporis odio, nesciunt sed eveniet, nemo amet, dignissimos magnam itaque ipsam possimus. Inventore vitae, perferendis harum quas cupiditate adipisci repellendus tempore asperiores at consequatur quos?</p>
-        </div>
-    )
-}
+  return (
+    <div className="about-content">
+      <p>This is an about component</p>
+      <p>
+        MyTodosList is a simple and intuitive todo list application built with
+        React. Designed to help users efficiently manage their tasks, the app
+        allows you to easily add, delete, and organize your todos. With a clean
+        and responsive interface, you can access your tasks from any device. Key
+        features include: Add and Delete Todos: Quickly manage your tasks with a
+        user-friendly form. List View: View all your todos in an organized
+        manner. Routing: Navigate seamlessly between the main todo list and an
+        About page. Whether you’re tracking daily chores or long-term projects,
+        MyTodosList aims to enhance your productivity and keep you organized.
+      </p>
+    </div>
+  );
+};
